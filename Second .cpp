ad55d2c379aa50175676522903a4e3928233fb84
Victor-janu <victorhaule13@gmail.com>
2025-03-24 02:48:58 +0300

    Add files via upload

---
+++ Second .cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+struct Record {
+    int id;
+    string name;
+};
+
+int main() {
+    cout << "Welcome to CRUD System in C++" << endl;
+    return 0;
+} 
